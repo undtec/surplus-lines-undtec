c8e8fcf2ba3c7e501a9abfce025f21c9473aa883
Mai Fathy <mfathy@undtec.com>
2026-01-20 14:14:35 +0200

    Fix: Add index.ts and update package.json with correct package name

---
+++ index.ts
@@ -0,0 +1,5 @@
+// Entry point for n8n community node
+// This file is required by package.json "main" field
+
+export * from './credentials/SurplusLinesApi.credentials';
+export * from './nodes/SurplusLinesApi/SurplusLinesApi.node';
